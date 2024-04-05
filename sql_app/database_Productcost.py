5c4e02ad69668a4de8bd5791f6fe70c0976a6efd
crazygeekcolin <115450713+crazygeekcolin@users.noreply.github.com>
2024-04-05 15:47:50 +0800

    0405

--- sql_app/database_Productcost.py
+++ sql_app/database_Productcost.py
@@ -7,15 +7,11 @@ import json
 
 db= SessionLocal()
 #results = db.query(models.Products.产品名称, models.Products.产品编号).all()
-results = db.query(models.Products.产品编号).all()
+#results = db.query(models.Products.产品编号).all()
 
 #Create a list with Primary KEY 
-result_list =[]
-for i in results:
-    result_list.append(i[0])
-print(result_list)
 
-df = pd.read_excel('../产品编号.xlsx', index_col=0)
+df = pd.read_excel('../产品成本.xlsx', index_col=0)
 ##Data operation
 def strip_series(series):
     ###Loop over items of pandas series and move their blank space in head and tail
@@ -26,31 +22,27 @@ def strip_series(series):
 for i in range(len(df)):
     strip_series(df.iloc[i])
 
-print(df.loc[2])
+result_list = []
 
 print(df.iloc[2].to_json(force_ascii=False))
 
-def add_db(row, primary_key_check: str):
+def add_db(row, primary_key_check = None):
     #Get the json format, need to convert later to sqlalchemy models
     json_string= row.to_json(force_ascii=False)
     js = json.loads(json_string)
     print(js, 'add_db')
+    return js
     
-    if js[primary_key_check] not in result_list:
-        return js
-
-
-
+    
 for i in range(len(df)):
-    row = add_db(df.iloc[i], primary_key_check= '产品编号')
+    row = add_db(df.iloc[i])
     if row:
-        db_product= models.Products(**row)
-        db.add(db_product)
+        db_product_cost= models.ProductsCost(**row)
+        db.add(db_product_cost)
         db.commit()
-        db.refresh(db_product)
-        
+        db.refresh(db_product_cost)
 
-print(add_db(df.iloc[4], primary_key_check='产品编号'))
+add_db(df.iloc[4])
 """ for i in range(len(df)):
     
     if df.iloc[i]['产品编号'] not in result_list:
